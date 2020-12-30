867346ae10cedcf4ddd74267d257bb13af6903d2
1800011848 <1800011848@login01.hpc.pku.edu.cn>
2020-12-30 23:58:01 +0800

    reform

--- const.py
+++
@@ -1,6 +0,0 @@
-class _const:
-    class ConstError(TypeError):pass
-    def __setattr__(self,name,value):
-        if name in self.__dict__:
-            raise self.ConstError("Can't rebind const (%s)" %name)
-        self.__dict__[name]=value

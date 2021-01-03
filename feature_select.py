df0d1faec5db6b08f5650a6828339a5f0fd65fd3
unknown <1800011714@pku.edu.cn>
2021-01-03 11:08:02 +0800

    feature select 2.0

--- feature_select.py
+++ feature_select.py
@@ -12,8 +12,10 @@ from sklearn.metrics import precision_score
 from sklearn.feature_selection import SelectKBest, RFE
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
+from sklearn.tree import plot_tree
 
-from header import CV_author, PUK_kernel, numout2boolout
+from header import CV_author, PUK_kernel, numout2boolout, reinterpret
+
 
 
 Xx = np.load("./processedData/Xx.npy")
@@ -25,9 +27,14 @@ x = Xx[len(Y):, :]
 X_feature_name = np.load("./processedData/X_featureName.npy")
 
 # 使用SelectKBest方法进行特征选择()
-def use_SelectKBest(n, print_log=True):
-    """n: int, # of features to be selected
-    print_log: bool"""
+def use_SelectKBest(n, print_log=True, interpret_tree=False):
+    """in:
+    n: int, # of features to be selected
+    print_log: bool
+    out:
+    precision: float, precision_score of svm using choosed features
+    selected: array(bool array), shape(n_features), True for features selected else False
+    """
     skb = SelectKBest(k=n)
     X_trans = skb.fit_transform(X, Y)
     selected = skb.get_support()
@@ -40,23 +47,38 @@ def use_SelectKBest(n, print_log=True):
     pred=model.predict(skb.transform(x))
     precision = precision_score(numout2boolout(y),numout2boolout(pred))
     if print_log:
-        print('precision={%.3f}'.format(precision))
-    return precision
+        print('precision={0:.3f}'.format(precision))
+
+    if interpret_tree:
+        tree = reinterpret(X_trans, model, X_trans)
+        plot_tree(tree, max_depth=5, feature_names=X_feature_name[selected])
+        plt.savefig("./decision_tree_skb.jpg")
+    
+    return precision, selected
+# features selected by SelectKBest:     
+# ['XXXinorg1' 'XXXinorg2' 'temp' 'slowCool' 'PaulingElectronegMean'
+#  'PaulingElectronegGeom' 'purity']    
+# precision=0.865
 
 # 确定合适的特征保留个数
 def decide_num():
     precisions = []
     for n in range(1, 10):
-        precisions.append(use_SelectKBest(n, print_log=False))
+        precisions.append(use_SelectKBest(n, print_log=False)[0])
     plt.plot(np.arange(1, 10), precisions, marker='.')
     plt.xlabel("# features")
     plt.ylabel("precision")
     plt.savefig("./precision_skb.jpg")
 
 # 使用RFE(recursive feature elimination)方法进行特征选择
-def use_RFE(n, print_log=True):
-    """n: int, # of features to be selected
-    print_log: bool"""
+def use_RFE(n, print_log=True, interpret_tree=False):
+    """in:
+    n: int, # of features to be selected
+    print_log: bool
+    out:
+    precision: float, precision_score of svm using choosed features
+    selected: array(bool array), shape(n_features), True for features selected else False
+    """
     rfe = RFE(estimator=GradientBoostingClassifier(), n_features_to_select=n)
     X_trans = rfe.fit_transform(X, Y)
     selected = rfe.get_support()
@@ -69,12 +91,18 @@ def use_RFE(n, print_log=True):
     pred=model.predict(rfe.transform(x))
     precision = precision_score(numout2boolout(y),numout2boolout(pred))
     if print_log:
-        print('precision={%0.3f}'.format(precision))
-    return precision
+        print('precision={0:.3f}'.format(precision))
+
+    if interpret_tree:
+        tree = reinterpret(X_trans, model, X_trans)
+        plot_tree(tree, max_depth=5, feature_names=X_feature_name[selected])
+        plt.savefig("./decision_tree_skb.jpg")
+    
+    return precision, selected
 # features choosed by RFE:
 # ['XXXinorg2mass' 'XXXoxlike1mass' 'time' 'orgASA_HGeomAvg'
 #  'PaulingElectronegMean' 'hardnessMinWeighted' 'purity']
-# precision= 0.8145454545454546
+# precision= 0.815
 
 if __name__ == "__main__":
-    decide_num()
+    use_SelectKBest(n=7, interpret_tree=True)
